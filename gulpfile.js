c1ada61cf4e20edbc407c91692b9f3038d5b8234
Rodrigo Reis <digo.santos.reis@gmail.com>
2017-09-25 11:34:08 -0300

    Adição do ngAnnotate no gulp para mimificar corretamente o angular

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var gulp = require('gulp'),
   notify = require('gulp-notify'),
   uglifycss = require('gulp-uglifycss'),
   imagemin = require('gulp-imagemin'),
-  htmlmin = require('gulp-htmlmin')
+  htmlmin = require('gulp-htmlmin'),
+  ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('minify-js', function() {
   return gulp.src('js/**')
+	.pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
 });
