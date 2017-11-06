87c841c60bc654214d22613573b2339af92d8af6
Tarso Matos <tarsomatos@gmail.com>
2017-11-06 09:28:29 -0300

    PAASMANUT-2002 Erro na criação do projeto Mobile RAD sem back-end

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,7 @@
     app.controller('HomeController', ['$scope', '$http', '$rootScope', '$state', '$translate', 'Notification', function ($scope, $http, $rootScope, $state, $translate, Notification) {
       $rootScope.http = $http;
 	    app.registerEventsCronapi($scope, $translate);
+	    
       for(var x in app.userEvents)
           $scope[x]= app.userEvents[x].bind($scope);
         $scope.message = {};
