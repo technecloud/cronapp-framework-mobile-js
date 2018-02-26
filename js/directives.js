af231c638da35844f8b87a197d22bc6b4154266e
Rodrigo Reis <digo.santos.reis@gmail.com>
2018-02-26 11:50:05 -0300

    Componente de QR Code, incremento para 0.0.58-d-s-b

--- js/directives.js
+++ js/directives.js
@@ -209,6 +209,119 @@
       }
     }])
 
+	.directive('qr', ['$window', function($window){
+		return {
+		  restrict: 'A',
+		  require: '^ngModel',
+		  template: '<canvas ng-hide="image"></canvas><img ng-if="image" ng-src="{{canvasImage}}"/>',
+		  link: function postlink(scope, element, attrs, ngModel){
+			if (scope.size === undefined  && attrs.size) {
+			  scope.text = attrs.size;
+			}
+		  var getTypeNumeber = function(){
+		  return scope.typeNumber || 0;
+		};
+		var getCorrection = function(){
+		  var levels = {
+			'L': 1,
+			'M': 0,
+			'Q': 3,
+			'H': 2
+		  };
+		var correctionLevel = scope.correctionLevel || 0;
+		  return levels[correctionLevel] || 0;
+		};
+		var getText = function(){
+		  return ngModel.$modelValue || "";
+		};
+		var getSize = function(){
+		  return scope.size || $(element).outerWidth();
+		};
+		var isNUMBER = function(text){
+		  var ALLOWEDCHARS = /^[0-9]*$/;
+		  return ALLOWEDCHARS.test(text);
+		};
+		var isALPHA_NUM = function(text){
+		  var ALLOWEDCHARS = /^[0-9A-Z $%*+\-./:]*$/;
+		  return ALLOWEDCHARS.test(text);
+		};
+		var is8bit = function(text){
+		  for (var i = 0; i < text.length; i++) {
+			var code = text.charCodeAt(i);
+			if (code > 255) {
+			  return false;
+			}
+		  }
+		  return true;
+		};
+		var checkInputMode = function(inputMode, text){
+		  if (inputMode === 'NUMBER' && !isNUMBER(text)) {
+			throw new Error('The `NUMBER` input mode is invalid for text.');
+		  }
+		  else if (inputMode === 'ALPHA_NUM' && !isALPHA_NUM(text)) {
+			throw new Error('The `ALPHA_NUM` input mode is invalid for text.');
+		  }
+		  else if (inputMode === '8bit' && !is8bit(text)) {
+			throw new Error('The `8bit` input mode is invalid for text.');
+		  }
+		  else if (!is8bit(text)) {
+			throw new Error('Input mode is invalid for text.');
+		  }
+		  return true;
+		};
+		var getInputMode = function(text){
+		  var inputMode = scope.inputMode;
+		  inputMode = inputMode || (isNUMBER(text) ? 'NUMBER' : undefined);
+		  inputMode = inputMode || (isALPHA_NUM(text) ? 'ALPHA_NUM' : undefined);
+		  inputMode = inputMode || (is8bit(text) ? '8bit' : '');
+		  return checkInputMode(inputMode, text) ? inputMode : '';
+		};
+		var canvas = element.find('canvas')[0];
+		var canvas2D = !!$window.CanvasRenderingContext2D;
+		scope.TYPE_NUMBER = getTypeNumeber();
+		scope.TEXT = getText();
+		scope.CORRECTION = getCorrection();
+		scope.SIZE = getSize();
+		scope.INPUT_MODE = getInputMode(scope.TEXT);
+		scope.canvasImage = '';
+		var draw = function(context, qr, modules, tile){
+		  for (var row = 0; row < modules; row++) {
+			for (var col = 0; col < modules; col++) {
+			  var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
+				  h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
+			  context.fillStyle = qr.isDark(row, col) ? '#000' : '#fff';
+			  context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
+			}
+		  }
+		};
+		var render = function(canvas, value, typeNumber, correction, size, inputMode){
+		  var trim = /^\s+|\s+$/g;
+		  var text = value.replace(trim, '');
+		  debugger;
+		  var qr = new QRCode(typeNumber, correction, inputMode);
+		  qr.addData(text);
+		  qr.make();
+		  var context = canvas.getContext('2d');
+		  var modules = qr.getModuleCount();
+		  var tile = size / modules;
+		  canvas.width = canvas.height = size;
+		  if (canvas2D) {
+			draw(context, qr, modules, tile);
+			scope.canvasImage = canvas.toDataURL() || '';
+		  }
+		};
+		
+		scope.$watch(function(){return ngModel.$modelValue}, function(value, old){
+		if (value !== old) {
+		  scope.text = ngModel.$modelValue;
+		  scope.TEXT = getText();
+		  scope.INPUT_MODE = getInputMode(scope.TEXT);
+		  render(canvas, scope.TEXT, scope.TYPE_NUMBER, scope.CORRECTION, scope.SIZE, scope.INPUT_MODE);
+		}
+	  });
+		render(canvas, scope.TEXT, scope.TYPE_NUMBER, scope.CORRECTION, scope.SIZE, scope.INPUT_MODE);
+		}};
+	  }])
 
     /**
      * Validação de campos CPF e CNPJ,
