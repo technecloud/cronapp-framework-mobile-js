aa46d757feb14a7dc8aff0c6aaaef26f9db969ca
henriquechang-cronapp <henrique.queiroz@cronapp.io>
2019-08-19 14:12:59 -0300

    [QAIBT-1414] - Adicionando comentários

--- js/directives.js
+++ js/directives.js
@@ -1506,6 +1506,7 @@ function maskDirective($compile, $translate, attrName, $parse) {
 
         $(element).inputmask(inputmaskType, ipOptions);
 
+        //Forçando um set no model no evento de keyup.
         var unmaskedvalue = function(event) {
           var rawValue = $(this).inputmask('unmaskedvalue');
           $(this).data('rawvalue',rawValue);
@@ -1521,6 +1522,7 @@ function maskDirective($compile, $translate, attrName, $parse) {
 
         if (ngModelCtrl) {
           ngModelCtrl.$formatters.push(function (value) {
+            //Ignorar a formatação pela máscara na primeira vez
             if (element._ignoreFormatter) {
               element._ignoreFormatter = false;
               return $(element).val();
