e63df3a43719d849f208004c09c223915c30a170
henriquechang-cronapp <henrique.queiroz@cronapp.io>
2019-08-21 14:34:28 -0300

    [QAIBT-1414] - Prefixo e sufixo para máscara de money decimal

--- js/directives.js
+++ js/directives.js
@@ -1486,9 +1486,10 @@ function maskDirective($compile, $translate, attrName, $parse) {
           ipOptions['groupSeparator'] = thousands;
         }
 
+
         Inputmask.extendAliases({
           "isNumeric": {
-            mask: "(.999){+|1},99",
+            mask: prefix + "(.999){+|1},99" + suffix,
             numericInput: true,
             placeholder: "0",
             rightAlign: true,
@@ -1501,7 +1502,10 @@ function maskDirective($compile, $translate, attrName, $parse) {
             },
             onKeyDown: function(event, buffer, caretPos, opts){
               return event;
-            }
+            },
+            allowMinus: false,
+            radixPoint: decimal,
+            digits: precision
           }
         });
 
