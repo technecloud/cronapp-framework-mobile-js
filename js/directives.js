fad224f51afef49b0989af44c2a6237d173b4644
Thiago Costa <thiagoalvescosta@gmail.com>
2020-03-10 16:30:56 -0300

    CRONAPP-1345 - Suporte a diretiva src-lazy

--- js/directives.js
+++ js/directives.js
@@ -1277,6 +1277,9 @@ window.addEventListener('message', function(event) {
           }
 
           scope.listButtonClick = function(idx, rowData, fn, event) {
+            if (event.target.tagName == 'INPUT') {
+              return;
+            }
 
             dataSource.goTo(rowData);
 
@@ -1634,8 +1637,159 @@ window.addEventListener('message', function(event) {
       }
   }])
 
+  .directive('srcLazy', function($compile){
+    'use strict';
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var $element = $(element);
+
+        if ($element.is(':appeared')) {
+          element.attr('src', attrs.srcLazy);
+          return;
+        }
+
+        if (!attrs.srcLazyPreview) {
+          $element.css('visibility', 'hidden');
+        } else {
+          $element.attr('src', attrs.srcLazyPreview);
+        }
+
+        $element.appear();
+
+        $element.on('appear', function(event, $all_appeared_elements) {
+          if (!$element.attr('src-lazy')) {
+            return;
+          }
+          if (!attrs.srcLazyPreview) {
+            $element.css('visibility', 'visible');
+          }
+          console.log(attrs.srcLazy);
+          $element.attr('src', attrs.srcLazy);
+          $element.attr('src-lazy', '');
+        });
+      }
+    }
+  })
+
 }(app));
 
+(function ($) {
+  var selectors = [];
+
+  var checkBinded = false;
+  var checkLock = false;
+  var defaults = {
+    interval: 250,
+    force_process: false
+  };
+  var $window = $(window);
+
+  var $priorAppeared = [];
+
+  function isAppeared() {
+    return $(this).is(':appeared');
+  }
+
+  function isNotTriggered() {
+    return !$(this).data('_appear_triggered');
+  }
+
+  function process() {
+    checkLock = false;
+
+    for (var index = 0, selectorsLength = selectors.length; index < selectorsLength; index++) {
+      var $appeared = $(selectors[index]).filter(isAppeared);
+
+      $appeared
+        .filter(isNotTriggered)
+        .data('_appear_triggered', true)
+        .trigger('appear', [$appeared]);
+
+      if ($priorAppeared[index]) {
+        var $disappeared = $priorAppeared[index].not($appeared);
+        $disappeared
+          .data('_appear_triggered', false)
+          .trigger('disappear', [$disappeared]);
+      }
+      $priorAppeared[index] = $appeared;
+    }
+  }
+
+  function addSelector(selector) {
+    selectors.push(selector);
+    $priorAppeared.push();
+  }
+
+  // ":appeared" custom filter
+  $.expr.pseudos.appeared = $.expr.createPseudo(function (_arg) {
+    return function (element) {
+      var $element = $(element);
+
+      var windowLeft = $window.scrollLeft();
+      var windowTop = $window.scrollTop();
+      var offset = $element.offset();
+      var left = offset.left;
+      var top = offset.top;
+
+      if (top + $element.height() >= windowTop &&
+          top - ($element.data('appear-top-offset') || 0) <= windowTop + $window.height() &&
+          left + $element.width() >= windowLeft &&
+          left - ($element.data('appear-left-offset') || 0) <= windowLeft + $window.width()) {
+        return true;
+      }
+      return false;
+    };
+  });
+
+  $.fn.extend({
+    // watching for element's appearance in browser viewport
+    appear: function (selector, options) {
+      $.appear(this, options);
+      return this;
+    }
+  });
+
+  $.extend({
+    appear: function (selector, options) {
+      var opts = $.extend({}, defaults, options || {});
+
+      if (!checkBinded) {
+        var onCheck = function () {
+          if (checkLock) {
+            return;
+          }
+          checkLock = true;
+
+          setTimeout(process, opts.interval);
+        };
+
+        $("ion-content").scroll(onCheck).resize(onCheck);
+        checkBinded = true;
+      }
+
+      if (opts.force_process) {
+        setTimeout(process, opts.interval);
+      }
+
+      addSelector(selector);
+    },
+    // force elements's appearance check
+    force_appear: function () {
+      if (checkBinded) {
+        process();
+        return true;
+      }
+      return false;
+    }
+  });
+}(function () {
+  if (typeof module !== 'undefined') {
+    // Node
+    return require('jquery');
+  }
+  return jQuery;
+}()));
 
 function maskDirectiveAsDate($compile, $translate, $parse) {
   return maskDirective($compile, $translate, 'as-date', $parse);
