2582746db1d44344acb3d306a26cd7b251a0e2fe
tiagoromano <tiago.romano@gmail.com>
2020-05-05 08:52:10 -0300

    CRONAPP-1288 - Uso de idiomas extensões do Português (ex: pt_PT) (#147)

--- js/app.authentication.js
+++ js/app.authentication.js
@@ -261,6 +261,7 @@ var app = (function() {
             });
 
             var locale = (window.navigator.userLanguage || window.navigator.language || 'pt_br').replace('-', '_');
+            locale = locale === 'pt_br' || locale === 'en_us' ? locale : 'pt_br';
 
             $translateProvider.use(locale.toLowerCase());
             $translateProvider.useSanitizeValueStrategy('escaped');
