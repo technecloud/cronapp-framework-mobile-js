8a2d3a33f33714227cb18717856e9e1f3a29d724
lincolnminto-cronapp <36731613+lincolnminto-cronapp@users.noreply.github.com>
2020-10-05 08:07:02 -0300

    CRONAPP-1565 (#196)
    Relatório não abre em visualização no browser do Android

--- js/reports/reports.service.js
+++ js/reports/reports.service.js
@@ -50,31 +50,7 @@
     
     // open report
     this.openURLContent = function(url) {
-      var include = function() {
-        var frame = $('<iframe/>');
-        frame.attr('frameborder', 0);
-        var h = parseInt($(window).height());
-        
-        frame.attr('height', h - 200);
-        frame.attr('width', '100%');
-        frame.attr('src', url + "?download=false");
-        var m = $('#reportView .modal-body');
-        if(m.get(0)) {
-          m.html(frame);
-          $('#reportViewContext .modal-dialog').css('width', '95%');
-          setTimeout(function() {
-            console.log('open[#reportViewContext]');
-            $('body').append(context);
-            $('#reportView').modal();
-          }, 100);
-        }
-        else {
-          console.log('wait[#reportViewContext]');
-          setTimeout(include, 200);
-        }
-      }
-      
-      setTimeout(include, 200);
+      openReportOnMobile(url)
     };
     
     this.initializeStimulsoft = function(language) {
@@ -454,6 +430,10 @@
         }
       }.bind(this));
     };
-    
+
+    function openReportOnMobile(url) {
+      window.open(url, '_system');
+    }
+
   });
 }(app));
